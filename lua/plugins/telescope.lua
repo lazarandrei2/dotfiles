05553649c2b23b2276c92ca35c4a652437ac245d
Andrei Lazar <lazarandrei0395@gmail.com>
2024-03-06 14:20:25 +0200

    vertical layout for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,15 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
 
         config = function()
+			require('telescope').setup({
+				defaults = {
+					layout_strategy = 'vertical',
+					layout_config = {
+						vertical = { width = 0.8 }
+					},
+				},
+			})
+
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
