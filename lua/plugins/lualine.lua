6e645f94450758fa219387e28bb688bb37728074
Andrei Lazar <lazarandrei0395@gmail.com>
2024-04-15 21:47:51 +0300

    clean the project

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,6 @@ return {
 	"nvim-lualine/lualine.nvim",
 
 	config = function()
-		require("lualine").setup({
-			options = {
-				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "", right = "" },
-			},
-		})
+		require("lualine").setup()
 	end,
 }
