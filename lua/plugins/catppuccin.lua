61aa187b136fabf381c4765ac6ab14eeb04897a4
lazarandrei2 <lazarandrei2@proton.me>
2024-09-10 09:47:30 +0300

    new themes and plugins

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,9 +4,8 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha",
-			transparent_background = true
 		})
 
-        vim.cmd.colorscheme("catppuccin")
+		-- vim.cmd.colorscheme("catppuccin")
 	end,
 }
