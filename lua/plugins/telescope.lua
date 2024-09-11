7636d813199b8b5e4a863ecaefc70a1a2d2a3f37
lazarandrei2 <lazarandrei2@proton.me>
2024-09-11 09:11:42 +0300

    neo-tree and autopairs plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { 
+			"nvim-lua/plenary.nvim",
+			{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+		},
 
 		config = function()
 			require("telescope").setup({
